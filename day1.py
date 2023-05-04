1d5670bca2a4f2f4e14c90dfb2418276a9823bb1
Creativestratnow <thiradejr@gmail.com>
2023-05-04 20:18:11 +0700

    test commit

---
+++ day1.py
@@ -0,0 +1,31 @@
+import math as m
+from math import sqrt, floor, ceil, pow
+from math import *
+
+
+
+
+
+
+
+
+
+
+
+
+import os
+# Operating System
+
+#PATHING
+'text.txt'
+'hi/hello.py'
+
+#check ว่าไฟล์มีอยู่จริง
+print(os.path.exists('hi/hello.py'))
+#check ว่าไฟล์มีอยู่จริงไหม
+os.path.isdir('hi')
+os.path.exists('hi')
+# list ว่าในโฟลเดอร์นี้มีอะไรบ้าง
+print(os.listdir('jedi'))
+#สร้างโฟลเดอร์
+#
